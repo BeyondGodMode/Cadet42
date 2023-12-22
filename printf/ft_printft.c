2de2267309050296494b625b787625722d883f7c
Tanakorn Songtrai <tsongtra@z2t5c2.42bangkok.com>
2023-12-22 11:25:40 +0700

    start

--- printf/ft_printft.c
+++ printf/ft_printft.c
@@ -6,8 +6,25 @@
 /*   By: tsongtra <tsongtra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 16:12:32 by tsongtra          #+#    #+#             */
-/*   Updated: 2023/12/22 09:05:43 by tsongtra         ###   ########.fr       */
+/*   Updated: 2023/12/22 11:23:02 by tsongtra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_printf()
+#include <stdio.h>
+#include <stdarg.h>
+
+int	ft_printf(const char *str, ...)
+{
+	va_list	args;
+
+	va_start(args, str);
+	printf("%s\n",str);
+	printf("%.2f\n",va_arg(args, double));
+	printf("%d",va_arg(args, int));
+	return (0);
+}
+
+int	main(void)
+{
+	ft_printf("test",1.1,2,3);
+}
